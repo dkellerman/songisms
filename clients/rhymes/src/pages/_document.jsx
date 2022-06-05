f8f5f57739a49f8224966540bca421380427f45f
David Kellerman <david@silvermoon.org>
2022-06-05 12:22:14 -0400

    ..

--- clients/rhymes/src/pages/_document.jsx
+++ clients/rhymes/src/pages/_document.jsx
@@ -6,14 +6,12 @@ export default function Document(ctx) {
       <Head>
         <link href="https://unpkg.com/papercss@1.8.3/dist/paper.min.css" rel="stylesheet" key="stylelib" />
         <script src="https://www.googletagmanager.com/gtag/js?id=UA-158752156-1"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { window.dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', 'UA-158752156-1');
-          `}
-        </script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { window.dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'UA-158752156-1');
+        `}}></script>
       </Head>
       <body>
         <Main />
