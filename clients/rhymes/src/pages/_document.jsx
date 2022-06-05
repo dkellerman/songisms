7f3217d4b7c35a302548ea36e956d2c917599992
David Kellerman <david@silvermoon.org>
2022-06-05 12:16:06 -0400

    gtm

--- clients/rhymes/src/pages/_document.jsx
+++ clients/rhymes/src/pages/_document.jsx
@@ -1,20 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document(ctx) {
   return (
     <Html>
       <Head>
         <link href="https://unpkg.com/papercss@1.8.3/dist/paper.min.css" rel="stylesheet" key="stylelib" />
-        <Script src="https://www.googletagmanager.com/gtag/js?id=UA-158752156-1" strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <script src="https://www.googletagmanager.com/gtag/js?id=UA-158752156-1"></script>
+        <script>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag() { window.dataLayer.push(arguments); }
             gtag('js', new Date());
             gtag('config', 'UA-158752156-1');
           `}
-        </Script>
+        </script>
       </Head>
       <body>
         <Main />
