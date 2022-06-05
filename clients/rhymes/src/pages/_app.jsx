3d5e21a490334915980846389b3ff14241cc71c5
David Kellerman <david@silvermoon.org>
2022-06-05 12:39:25 -0400

    gtag

--- clients/rhymes/src/pages/_app.jsx
+++ clients/rhymes/src/pages/_app.jsx
@@ -1,10 +1,21 @@
 import Layout from '../components/Layout';
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-158752156-1" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { window.dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'UA-158752156-1');
+      `}}></Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
