f4d9861db09d3cc1f0bb7927eac459d1c9d522b1
David Kellerman <david@silvermoon.org>
2022-06-05 12:49:17 -0400

    prettier

--- clients/rhymes/src/pages/_app.jsx
+++ clients/rhymes/src/pages/_app.jsx
@@ -1,16 +1,22 @@
 import Layout from '../components/Layout';
-import Script from "next/script";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script src="https://www.googletagmanager.com/gtag/js?id=UA-158752156-1" strategy="afterInteractive"></Script>
-      <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag() { window.dataLayer.push(arguments); }
         gtag('js', new Date());
         gtag('config', 'UA-158752156-1');
-      `}}></Script>
+      `,
+        }}
+      ></Script>
 
       <Layout>
         <Component {...pageProps} />
