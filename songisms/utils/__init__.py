ad0e989b3b397f4a3eb0da2275dfa5d321e07cdd
David Kellerman <david@silvermoon.org>
2023-08-28 02:50:01 -0400

    ..

--- songisms/utils/__init__.py
+++ songisms/utils/__init__.py
@@ -1,7 +1,10 @@
-from .cloud import *
-from .data import *
-from .text import *
-from .ipa import *
-from .ml import *
-from .gql import *
-from .gpt import *
+try:
+    from .cloud import *
+    from .data import *
+    from .text import *
+    from .ipa import *
+    from .ml import *
+    from .gql import *
+    from .gpt import *
+except ImportError:
+    pass
